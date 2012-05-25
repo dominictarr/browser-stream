4e3e88bfe91be6b07a4c63f28b6fb4a321787c76
Dominic Tarr <dominic.tarr@gmail.com>
2012-05-26 07:21:12 +1200

    allow multiple streams with the same name, and support pause and resume

--- index.js
+++ index.js
@@ -4,37 +4,61 @@ var EventEmitter = require('events').EventEmitter
 
 module.exports = function (sock) {
   var e = new EventEmitter ()
-
+  var count = 1
   //id use socket.io namespaces here, but they are really arwkward in this usecase.
+  var DATA   = ''
+    , END    = '.'
+    , PAUSE  = '?'
+    , RESUME = '*'
+    , ERROR  = '!'
+
   function _writeStream (s) {
-      var DATA = s.name
-      var END = 'END_'+s.name
-       s.write = function (data) {
-        sock.emit(DATA, data)
-        return true
-      }
-      s.end = function (data) {
-        if(data != null) this.write(data)
-        sock.emit(END)
-      }
-      //sock.on('PAUSE_'+name, ...
-      //sock.on('DRAIN_'+name, ... 
+    var id = s._id
+     s.write = function (data) {
+      sock.emit(DATA + id, data)
+      return !s.paused
+    }
+    s.end = function (data) {
+      if(data != null) this.write(data)
+      sock.emit(END + id)
+    }
+    sock.on(PAUSE + id,function () {
+      s.paused = true
+    })
+    sock.on(RESUME + id, function () {
+      s.paused = false
+      s.emit('drain')
+    })
+    //TODO but remember, some errors are circular.
+    //sock.on(ERROR + id, function (error) {
+    //  s.emit('error', error)
+    //}
   }
 
   function _readStream (s) {
-    var DATA = s.name
-      , END = 'END_'+s.name
+    var id = s._id
     s.readable = true
     function onData(data) {
       s.emit('data', data)
     }
     function onEnd () {
       s.emit('end')
-      sock.removeListener(DATA, onData)
-      sock.removeListener(END, onEnd)
+      sock.removeListener(DATA + id, onData)
+      sock.removeListener(END + id, onEnd)
     }
-    sock.on(DATA, onData)
-    sock.on(END, onEnd) 
+    sock.on(DATA + id, onData)
+    sock.on(END + id, onEnd)
+
+    s.pause = function () {
+      s.paused = true
+      sock.emit(PAUSE + id)
+    }
+    s.resume = function () {
+      s.paused = false
+      sock.emit(RESUME + id)
+    }
+    //s.on('error', function (error) {
+    //})
   }
 
   function _createStream(opts) {
@@ -46,6 +70,7 @@ module.exports = function (sock) {
     if(opts.opts || opts.options)
       s.options  = opts.opts || opts.options
     s.name = opts.name
+    s._id      = opts._id || count ++
     if(s.writable)
       _writeStream(s)
     if(s.readable)
