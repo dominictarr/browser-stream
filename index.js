7ac96f46cdf1d113bd3e8476b634f0d7b245ddd7
Dominic Tarr <dominic.tarr@gmail.com>
2012-06-02 02:08:39 +1200

    inline _writeStream and _readStream, because they share some events, and need to share references

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 
 var Stream = require('stream').Stream
 var EventEmitter = require('events').EventEmitter
+
 /*
 Although sockjs and browserchannel are consistent on the client
 they are different on the server, while socket.io are consistent on the client and the server...
 
-I think I gotta fit in the reconnector some place different...
-also, it's only the client side that should reconnect.
-the server shouldn't.
+I wrote a module that wraps sockjs (could easily adapt to wrap any WebSocket Compatibility Layer)
+in an interface that is like an two remote event emitters,
+except that A.on('event',listener) is triggered by B.emit('event')
+
+(see the tests)
+
+
+
 */
-//so I probably want to pass this a reconnector
-//that replicates the EventEmitter interface,
-//and emits 'disconnect', and then 'connect' when reconnected.
-//it buffers emitted events if not connected,
-//but all streams should end on disconnect.
-//feels like this shoud be a seperate module
 
 module.exports = function (sock) {
 
@@ -22,17 +22,13 @@ module.exports = function (sock) {
   socket.io is a bad pattern. it's a single lib,
   but it _should_ be a small ecosystem of tools.
 
-  gonna replace this with something that the other stream-fallback modules.
-  sockjs, and BrowserChannel. they are both pretty much the same...
-
-  when you call creatStream() it should return a stream immediately.
+  when you call createStream() it should return a stream immediately.
   thats how it is everywhere else in node.
   
   but, if there is a disconnection, all the streams should emit 'end'.
   then emit 'error' if you try to write to them after?
 
-  hmm, I think I want to wrap the stream in a EventEmitter interface.
-  and then turn that into the multiplexed stream.
+  my problem wasn't in socket.io... it was in browser-stream.
 */
 
   var e = new EventEmitter ()
@@ -42,57 +38,6 @@ module.exports = function (sock) {
     , PAUSE  = '?'
     , RESUME = '*'
     , ERROR  = '!'
-
-  function _writeStream (s) {
-    var id = s._id
-     s.write = function (data) {
-      sock.emit(DATA + id, data)
-      return !s.paused
-    }
-    s.end = function (data) {
-      if(data != null) this.write(data)
-      sock.emit(END + id)
-    }
-    sock.on(PAUSE + id,function () {
-      s.paused = true
-    })
-    sock.on(RESUME + id, function () {
-      s.paused = false
-      s.emit('drain')
-    })
-    //TODO but remember, some errors are circular.
-    //sock.on(ERROR + id, function (error) {
-    //  s.emit('error', error)
-    //}
-  }
-
-  function _readStream (s) {
-    var id = s._id
-    s.readable = true
-    function onData(data) {
-      s.emit('data', data)
-    }
-    function onEnd () {
-      s.emit('end')
-      sock.removeListener(DATA + id, onData)
-      sock.removeListener(END + id, onEnd)
-      sock.removeListener('disconnect', onEnd)
-    }
-    sock.on(DATA + id, onData)
-    sock.on(END + id, onEnd)
-    sock.on('disconnect', onEnd)
-
-    s.pause = function () {
-      s.paused = true
-      sock.emit(PAUSE + id)
-    }
-    s.resume = function () {
-      s.paused = false
-      sock.emit(RESUME + id)
-    }
-    //s.on('error', function (error) {
-    //})
-  }
 
   function _createStream(opts) {
     var s = new Stream()
@@ -104,10 +49,78 @@ module.exports = function (sock) {
       s.options  = opts.opts || opts.options
     s.name = opts.name
     s._id      = opts._id || count ++
-    if(s.writable)
-      _writeStream(s)
-    if(s.readable)
-      _readStream(s)
+
+    if(s.writable) {
+      var id = s._id
+       s.write = function (data) {
+        sock.emit(DATA + id, data)
+        return !s.paused
+      }
+
+      s.end = function (data) {
+        if(data != null) this.write(data)
+        sock.emit(END + id)
+        cleanup()
+      }
+      function onPause () {
+        s.paused = true
+      }
+      function onResume () {
+        s.paused = false
+        s.emit('drain')
+      }
+      function onDisconnect () {
+        console.log('DISCONNECT!!!!')
+        s.emit('close')
+        cleanup() 
+      }
+
+      sock.on(PAUSE + id, onPause)
+      sock.on(RESUME + id, onResume)
+      sock.on('disconnect', onDisconnect)
+
+      function cleanup () {
+        sock.removeListener(PAUSE + id, onPause)
+        sock.removeListener(RESUME + id, onResume)
+        sock.removeListener('disconnect', onDisconnect)
+      }
+      //TODO but remember, some errors are circular.
+      //sock.on(ERROR + id, function (error) {
+      //  s.emit('error', error)
+      //}
+
+    }
+    if(s.readable){
+      var id = s._id
+      s.readable = true
+      function onData(data) {
+        s.emit('data', data)
+      }
+      function onEnd () {
+        s.emit('end')
+        sock.removeListener(DATA + id, onData)
+        sock.removeListener(END + id, onEnd)
+        sock.removeListener('disconnect', onEnd)
+        //OH, I didn't understand about close.
+        //that means you can't write anymore.
+      }
+      sock.on(DATA + id, onData)
+      sock.on(END + id, onEnd)
+      sock.on('disconnect', onEnd)
+
+      s.pause = function () {
+        s.paused = true
+        sock.emit(PAUSE + id)
+      }
+      s.resume = function () {
+        s.paused = false
+        sock.emit(RESUME + id)
+      }
+      //s.on('error', function (error) {
+      //})
+
+    } //end of setting up readable stream
+
     return s
   }
   e.createWriteStream = function (name, opts) { 
