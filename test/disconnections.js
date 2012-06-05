a40dc9ff7200cef0e61a0ef1cb2117b1a26f8c04
Dominic Tarr <dominic.tarr@gmail.com>
2012-06-05 18:21:36 +1200

    updated tests for disconnections

--- test/disconnections.js
+++ test/disconnections.js
@@ -35,42 +35,54 @@ function randomNumberStream (max, count) {
 
   var client = bs(_client)
   var server = bs(_server)
-
+  var count = 0, dCount = 1
   server.on('connection', function (stream) {
     a.equal(stream.name, 'disconnect1')
     stream
-      .pipe(slave)
+      .on('error', function () {
+        console.log('<< ERROR')
+      })
+     .pipe(slave)
       .pipe(es.log('<<'))//.pipe(stream)
-      .on('end', function () {
+      .on('data', function () {
+        dCount ++
+      })
+     .on('end', function () {
+        a.equal(count, dCount, 'each stream should see the same items')
         console.log('<< END')
       })
-      .on('close', function () {
-        console.log('<< CLOSE')
-      })
   })
-  var count = 0
-  randomNumberStream()
-    .on('data', function () {
-      if(count ++ < 12) return
-      console.log('DISCONNECT')
-      if(_client.connected)
+  var rns = randomNumberStream()
+  rns
+    .on('data', function (data) {
+      if(++ count < 12) return
+      if(_client.connected) {
         _client.disconnect()
+        console.log('DISCONNECT')
+      }
+      console.log('DATA', data, count)
     })
     .pipe(master)
     .pipe(es.log('>>'))
-    .pipe(client.createWriteStream('disconnect1'))
-    .on('end', function () {
-      //END should always be EMITTED
-      //RIGHT?
-      console.log('>> END')
-      a.throws(function () { slave.validate() })
-    })
-    .on('close', function () {
-      console.log('>> CLOSE')
-    })
-  
+    .pipe(client.createWriteStream('disconnect1')
+      .on('error', function () {rns.destroy(); console.log('>> ERROR')})
+      .on('end', function () {
+        //END should always be EMITTED
+        //RIGHT?
+        a.equal(count, dCount, 'each stream should see the same items')
+        console.log('>> END')
+      //not all the events are emitted, 
+      //but since the streams are destroyed,
+      //and piping stops then they end up with 
+      //the same data through them.
+        slave.validate()
+      }))
+  /*
+  THERE are some problems with streams that close.
+  or rather, SHOULD close.   
+  */ 
+ 
   _client.connect() //connects the server too
-  console.log('ms')
 })();
 
 ;(function simple () {
@@ -108,7 +120,12 @@ function randomNumberStream (max, count) {
     new RemoteEventEmitter(
       _client = new RemoteEventEmitter()
     )
-
+  _client.on('disconnect', function () {
+    console.log('CLIENT DISCONNECT')
+  })
+  _server.on('disconnect', function () {
+    console.log('SERVER DISCONNECT')
+  })
   var client = bs(_client)
   var server = bs(_server)
   _client.connect() //connects the server too
@@ -119,42 +136,55 @@ function randomNumberStream (max, count) {
     randoms.push(r = Math.random())
     return r
   }
+  var clientErr = false, serverErr = false
+  process.on('exit', function () {
+    a.ok(clientErr, 'expected client to emit an error')
+    a.ok(serverErr, 'expected server to emit an error')
+    console.log('end point emitted errors correctly')
+  })
+
   var streams = 0, ended = 0
   server.on('connection', function (stream) {
     streams ++
-    stream.on('data', function (data) {
-      var r 
-      a.equal(data, r = randoms.shift())
-      console.log('data', r)
-    })
-    stream.on('close', function () {
-      a.equal(streams, 1)
-      console.log('close')
-    })
+    stream
+      .on('data', function (data) {
+        var r 
+        a.equal(data, r = randoms.shift())
+        console.log('data', r)
+      })
+      .on('error', function () {
+        //I'm expecting this
+        serverErr = true
+        a.equal(streams, 1)
+        console.log('error!')
+      })
+    var r = Math.random()
+    var _ended = false
     stream.on('end', function () {
+      a.ok(!_ended, 'end MUST only be emitted once')
+      _ended = true
       a.equal(streams, ++ ended)
-      console.log('end')
+      console.log('end!')
     })
   })
 
-  c = client.createWriteStream()
-  c.on('close', function () {
-    console.log('close')
+  var c = client.createWriteStream('A')
+  c.on('error', function () {
+    //expecting this!
+    clientErr = true
+    console.log('error')
   })
-  c.write(rand())
-  c.write(rand())
-  c.write(rand())
-  c.write(rand())
 
+  c.write(rand())
+  c.write(rand())
+  c.write(rand())
+  c.write(rand())
   _client.disconnect()
-  c2 = client.createWriteStream()
-  c.write(rand())
-  c.write(rand())
-  c.write(rand())
-  c.write(rand())
-  _client.connect()
-  c.end()
- 
+
+  if(c.writable)
+    c.write(rand())
+  a.throws(function () { c.write(rand()) })
+
 })();
 
 
@@ -181,7 +211,7 @@ console.log('disconnect2')
     s.write(rand())
     s.write(rand())
     s.end()
-    s.write(Math.random())//this should be ignored
+    a.throws(function () { s.write(Math.random()) })//this should be ignored
   })
 
   c = client.createReadStream()
